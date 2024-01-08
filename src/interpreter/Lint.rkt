a8a75e7410f8dede51c924e19ea29b77b4f62b56
thelissimus <thelissimus@tuta.io>
2024-01-09 00:49:39 +0500

    add(interp-Lint): recursive utils

--- src/interpreter/Lint.rkt
+++ src/interpreter/Lint.rkt
@@ -8,3 +8,21 @@
 (define (negate a)  (Prim '- (list a)))
 (define (minus a b) (Prim '- (list a b)))
 (define (plus a b)  (Prim '+ (list a b)))
+
+(define (leaf? ast)
+  (match ast
+    [(Int _) #t]
+    [(Prim 'read '()) #t]
+    [_ #f]))
+
+(define (exp? ast)
+  (match ast
+    [(Int _) #t]
+    [(Prim 'read '()) #t]
+    [(Prim _ args) (andmap exp? args)]
+    [_ #f]))
+
+(define (Lint? ast)
+  (match ast
+    [(Program '() e) (exp? e)]
+    [_ #f]))
