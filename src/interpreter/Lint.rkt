1deb043bbe8fac1a687973607e3d88d8f3af20f1
thelissimus <thelissimus@tuta.io>
2024-01-09 00:49:15 +0500

    add(interp-Lint): primitive ops

--- src/interpreter/Lint.rkt
+++ src/interpreter/Lint.rkt
@@ -3,3 +3,8 @@
 (struct Int (value))
 (struct Prim (op args))
 (struct Program (info body))
+
+(define rd          (Prim 'read '()))
+(define (negate a)  (Prim '- (list a)))
+(define (minus a b) (Prim '- (list a b)))
+(define (plus a b)  (Prim '+ (list a b)))
