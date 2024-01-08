efb17299b12fda3a33d4a3085573a0bcd859de71
thelissimus <thelissimus@tuta.io>
2024-01-09 01:15:59 +0500

    add(interp-Lint): interpreter

--- src/interpreter/Lint.rkt
+++ src/interpreter/Lint.rkt
@@ -1,14 +1,17 @@
 #lang racket
 
+;;; AST
 (struct Int (value))
 (struct Prim (op args))
 (struct Program (info body))
 
+;;; Primitives
 (define rd          (Prim 'read '()))
 (define (negate a)  (Prim '- (list a)))
 (define (minus a b) (Prim '- (list a b)))
 (define (plus a b)  (Prim '+ (list a b)))
 
+;;; Operations
 (define (leaf? ast)
   (match ast
     [(Int _) #t]
@@ -26,3 +29,19 @@
   (match ast
     [(Program '() e) (exp? e)]
     [_ #f]))
+
+;;; Interpreter
+(define (interp_exp e)
+  (match e
+    [(Int n) n]
+    [(Prim 'read '())
+     (let ([r (read)])
+       (cond [(fixnum? r) r]
+             [else (error 'interp_exp "read expected an integer: ~v" r)]))]
+    [(Prim '- (list a))   (- 0 (interp_exp a))]
+    [(Prim '- (list a b)) (- (interp_exp a) (interp_exp b))]
+    [(Prim '+ (list a b)) (+ (interp_exp a) (interp_exp b))]))
+
+(define (interp_Lint p)
+  (match p
+    [(Program '() e) (interp_exp e)]))
