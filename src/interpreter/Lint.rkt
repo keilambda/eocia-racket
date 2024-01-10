ca6215e7f3f93a2faf00afe8056b677f6d22b567
thelissimus <thelissimus@tuta.io>
2024-01-11 01:50:19 +0500

    add(interp-Lvar): interpreter

--- src/interpreter/Lint.rkt
+++ src/interpreter/Lint.rkt
@@ -3,6 +3,8 @@
 ;;; AST
 (struct Int (value))
 (struct Prim (op args))
+(struct Var (name))
+(struct Let (name value body))
 (struct Program (info body))
 
 ;;; Primitives
@@ -73,3 +75,35 @@
 (define (pe_Lint p)
   (match p
     [(Program '() e) (Program '() (pe_exp e))]))
+
+(define interp-Lint%
+  (class object%
+    (super-new)
+
+    (define/public (interp-exp env e)
+      (match e
+        [(Int n) n]
+        [(Prim 'read '())
+         (let ([r (read)])
+           (cond [(fixnum? r) r]
+                 [else (error 'interp-exp "read expected an integer: ~v" r)]))]
+        [(Prim '- (list e)) (- 0 (interp-exp env e))]
+        [(Prim '- (list a b)) (- (interp-exp env a) (interp-exp env b))]
+        [(Prim '+ (list a b)) (+ (interp-exp env a) (interp-exp env b))]))
+
+    (define/public (interp-program p)
+      (match p
+        [(Program '() e) (interp-exp '() e)]))))
+
+(define interp-Lvar%
+  (class interp-Lint%
+    (super-new)
+
+    (define/override (interp-exp env e)
+      (match e
+        [(Var name) (dict-ref env name)]
+        [(Let var rhs body) (interp-exp (dict-set env var (interp-exp env rhs)) body)]
+        [_ (super interp-exp env e)]))))
+
+(define (interp-Lvar p)
+  (send (new interp-Lvar%) interp-program p))
